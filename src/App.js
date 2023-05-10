6b498cbfa09a4edbfd91bd834c2cee729b17e54b
vlad <sp0ngbik@gmail.com>
2023-05-10 22:55:12 +0300

    switch elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import style from "./style.module.scss";
 function App() {
+  const [switcher, setSwither] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.switcherBlockBody}>
+      <div className={style.switherWay}>
+        <button
+          className={style.buttonForClick}
+          onClick={() => {
+            setSwither(!switcher);
+          }}
+        ></button>
+        <div
+          className={
+            switcher ? style.startMoveStartElement : style.startElement
+          }
+        ></div>
+        <div
+          className={switcher ? style.startMoveEndElement : style.endElement}
+        ></div>
+      </div>
     </div>
   );
 }
